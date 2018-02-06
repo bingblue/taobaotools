0c41a8eb86a8df000b5ae8134ff5aa0892c28d5e
XiaoMuCOOL <895355044@qq.com>
2018-02-06 11:25:04 +0800

    增加工具

---
+++ src/main/webapp/js/addMore.js
@@ -0,0 +1,27 @@
+'use strict'
+$(function () {
+  var instance = $('form').parsley()
+  /* 卡首屏手机端 */
+  $(".save").click(function(){
+    if(!instance.validate()){
+      return false
+    }
+    var option = {
+      url: config.api.addMore,
+      data: {
+        taobaoProductId: $.getQueryString('id', $('input[name=link]').val()),	// 淘宝商品Id
+        keywords: $('input[name=keyword]').val(),                             // 搜索的关键字
+        productTitle: $('input[name=title]').val(),                           // 淘口令分享标题
+        logoUrl: ''                                                           // 淘口令分享图片地址       
+      }
+    }
+    $.muAjax(option, function(data){
+      var reuslt = {
+        '淘口令': data.tpwd,
+        '卡首屏Url': data.url
+      }
+      $.bomb(reuslt)
+    })
+  })
+  
+})
--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -2,6 +2,10 @@ var url = 'http://bingblue.com:8000/TaobaoTools'
 // var url = 'http://localhost:8080'
 var config = {
   api : {
-    'mobile': url + '/user/stuck/mobile.do'
+    'mobile': url + '/user/stuck/mobile.do',
+    'competitor': url + '/user/stuck/competitor.do',
+    'create': url + '/api/taobao/tpwd/create.do',
+    'addOne': url + '/user/manyOrder/addOne.do',
+    'addMore': url + '/user/manyOrder/addMore.do' 
   }
 }
