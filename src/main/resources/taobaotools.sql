6b9024c164d933da930f480e02ee50c6c8b080ed
Say_Ming <sayming@bingblue.com>
2018-03-10 09:53:49 +0800

    sql add

---
+++ src/main/resources/taobaotools.sql
@@ -0,0 +1,239 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Home
+Source Server Version : 50524
+Source Host           : localhost:3306
+Source Database       : taobaotools
+
+Target Server Type    : MYSQL
+Target Server Version : 50524
+File Encoding         : 65001
+
+Date: 2018-03-10 09:51:41
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `t_authority`;
+CREATE TABLE `t_authority` (
+  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `root_url` varchar(255) DEFAULT NULL COMMENT 'url路径名',
+  `create_date_time` datetime NOT NULL COMMENT '创建时间',
+  `operation_date_time` datetime NOT NULL COMMENT '操作时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_authoritydetail
+-- ----------------------------
+DROP TABLE IF EXISTS `t_authoritydetail`;
+CREATE TABLE `t_authoritydetail` (
+  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `head_id` int(255) unsigned NOT NULL COMMENT '权限模块Id',
+  `name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL COMMENT '可访问的url路径',
+  `create_date_time` datetime NOT NULL COMMENT '创建时间',
+  `operation_date_time` datetime NOT NULL COMMENT '操作时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_localcookie
+-- ----------------------------
+DROP TABLE IF EXISTS `t_localcookie`;
+CREATE TABLE `t_localcookie` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `value` varchar(2048) DEFAULT NULL,
+  `expires_at` bigint(20) DEFAULT NULL,
+  `domain` varchar(255) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `secure` tinyint(1) DEFAULT NULL,
+  `http_only` tinyint(1) DEFAULT NULL,
+  `persistent` tinyint(1) DEFAULT NULL,
+  `host_only` tinyint(1) DEFAULT NULL,
+  `client_id` bigint(20) DEFAULT '0',
+  `type` varchar(255) DEFAULT NULL,
+  `host` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_manyorderbill
+-- ----------------------------
+DROP TABLE IF EXISTS `t_manyorderbill`;
+CREATE TABLE `t_manyorderbill` (
+  `id` bigint(255) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(255) unsigned NOT NULL DEFAULT '0',
+  `short_link` varchar(255) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `product_url` varchar(2048) DEFAULT NULL,
+  `member_id` bigint(255) unsigned NOT NULL DEFAULT '0' COMMENT '会员Id',
+  `status` varchar(50) DEFAULT NULL COMMENT '状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_manyorderdetail
+-- ----------------------------
+DROP TABLE IF EXISTS `t_manyorderdetail`;
+CREATE TABLE `t_manyorderdetail` (
+  `id` bigint(255) unsigned NOT NULL AUTO_INCREMENT,
+  `head_id` bigint(255) NOT NULL DEFAULT '0',
+  `keywords` varchar(255) DEFAULT NULL,
+  `limit_click_quantity` int(255) DEFAULT '0' COMMENT '限制点击量',
+  `click_quantity` int(255) DEFAULT '0' COMMENT '已点击量',
+  `click_count` int(255) DEFAULT '0' COMMENT '累计点击量',
+  `product_url` varchar(2048) DEFAULT NULL,
+  `taobao_word_id` bigint(20) DEFAULT NULL COMMENT '生成的淘口令Id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_member
+-- ----------------------------
+DROP TABLE IF EXISTS `t_member`;
+CREATE TABLE `t_member` (
+  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `create_date_time` datetime NOT NULL COMMENT '创建时间',
+  `operation_date_time` datetime NOT NULL COMMENT '操作时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `authority_id` int(255) DEFAULT NULL COMMENT '权限模块Id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_productcatchlist
+-- ----------------------------
+DROP TABLE IF EXISTS `t_productcatchlist`;
+CREATE TABLE `t_productcatchlist` (
+  `keywords` varchar(255) DEFAULT NULL,
+  `happen_date` date DEFAULT NULL,
+  `product_ids` varchar(3000) DEFAULT NULL,
+  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_stuckfirstscreenbill
+-- ----------------------------
+DROP TABLE IF EXISTS `t_stuckfirstscreenbill`;
+CREATE TABLE `t_stuckfirstscreenbill` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `keywords` varchar(255) DEFAULT NULL,
+  `product_id` varchar(255) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `member_id` bigint(20) DEFAULT NULL,
+  `taobao_word_id` bigint(20) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `competitor` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_taobaoshop
+-- ----------------------------
+DROP TABLE IF EXISTS `t_taobaoshop`;
+CREATE TABLE `t_taobaoshop` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `shop_id` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `lately_capture_date` datetime DEFAULT NULL,
+  `total_sale_quantity` int(11) DEFAULT NULL,
+  `total_sale` double(14,4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_taobaoshopcapturebill
+-- ----------------------------
+DROP TABLE IF EXISTS `t_taobaoshopcapturebill`;
+CREATE TABLE `t_taobaoshopcapturebill` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `member_id` bigint(20) DEFAULT NULL,
+  `shop_url` varchar(255) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `done_date` datetime DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `removed` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_taobaoshopproduct
+-- ----------------------------
+DROP TABLE IF EXISTS `t_taobaoshopproduct`;
+CREATE TABLE `t_taobaoshopproduct` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `p_id` varchar(255) DEFAULT NULL,
+  `price` double(14,4) DEFAULT NULL,
+  `total_sale_quantity` int(11) DEFAULT NULL,
+  `total_comment_quantity` int(11) DEFAULT NULL,
+  `total_sale` double(14,4) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `happen_date` datetime DEFAULT NULL,
+  `shop_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_taobaoword
+-- ----------------------------
+DROP TABLE IF EXISTS `t_taobaoword`;
+CREATE TABLE `t_taobaoword` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `status` varchar(50) DEFAULT NULL COMMENT '状态',
+  `text` varchar(255) DEFAULT NULL COMMENT '淘口令标题内容',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `logo_url` varchar(255) DEFAULT NULL,
+  `ext` varchar(255) DEFAULT NULL,
+  `url` varchar(2048) DEFAULT NULL,
+  `tpwd` varchar(255) DEFAULT NULL COMMENT '淘口令',
+  `failure_date` datetime DEFAULT NULL,
+  `member_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_taobaowordbill
+-- ----------------------------
+DROP TABLE IF EXISTS `t_taobaowordbill`;
+CREATE TABLE `t_taobaowordbill` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `create_date` datetime DEFAULT NULL,
+  `member_id` bigint(20) DEFAULT NULL,
+  `taobao_word_id` bigint(20) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `url` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `create_date_time` datetime NOT NULL COMMENT '创建时间',
+  `operation_date_time` datetime NOT NULL COMMENT '操作时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `authority_id` int(255) DEFAULT NULL COMMENT '权限Id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
