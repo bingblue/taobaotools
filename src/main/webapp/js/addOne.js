28000ebc688c7a671d827bb4f44fe7feb4c1f56d
XiaoMuCOOL <895355044@qq.com>
2018-03-10 19:17:35 +0800

    update

--- src/main/webapp/js/addOne.js
+++ src/main/webapp/js/addOne.js
@@ -24,7 +24,7 @@ $(function () {
       option.data['manyOrderDetails['+ i +'].limitClickQuantity'] = limitClickQuantity
     })
     $.muAjax(option, function(data){
-      $.tip('请到淘词补单列表查看')
+      $.tip('请到淘词补单列表查看',2000)
     })
   })
   
